3618e83c59ed9ab8028f9df2b5a9ce5e7b82e924
santhosh@ <jits.santhosh@gmail.com>
2018-12-25 02:16:32 +0530

    Signed-off-by: santhosh@ <jits.santhosh@gmail.com>

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
