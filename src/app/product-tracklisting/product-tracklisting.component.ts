d4ae66138a271116cdd537c85939533f976c4a44
santhosh@ <jits.santhosh@gmail.com>
2018-12-25 02:40:11 +0530

    Signed-off-by: santhosh@ <jits.santhosh@gmail.com>

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -16,7 +16,7 @@ albumInfo: Album;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response) => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
